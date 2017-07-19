acabcf3af45c512d4a389431d909a5ada83d226a
tsurrdurr <tsurrdurr@users.noreply.github.com>
2017-07-19 18:33:19 +0300

    Universialized the way of getting random question number. Added third extra question that is shown after user input.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 def main():
 	topics = []
@@ -7,11 +8,16 @@ def main():
 			topics.append(line);
 	maxvalue = len(topics) - 1
 	print("Questions found:", maxvalue)
-	rn = random.randint(0, maxvalue)
+	rn = get_non_dupe_question(topics, [])
 	rn2 = get_non_dupe_question(topics, [rn])
+	extra = get_non_dupe_question(topics, [rn, rn2])
 	print("Your randomly chosen non-duplicating questions: ")
 	print(topics[rn].strip())
 	print(topics[rn2].strip())
+
+	print("Press any key to get an extra question to answer without preparation: ")
+	os.system("pause")
+	print(topics[extra].strip())
 
 def get_non_dupe_question(topics, dupes):
 	maxvalue = len(topics) - 1
